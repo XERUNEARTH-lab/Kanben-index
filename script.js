acacd28a521a5ff75acd61085248598df8267afb
XERUNEARTH-lab <conohanna@gmail.com>
2026-06-18 23:57:47 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
+fetch("characters.json")
+  .then(response => response.json())
+  .then(characters => {
 
+    const container =
+      document.getElementById("character-list");
+
+    characters.forEach(character => {
+
+      const card =
+        document.createElement("div");
+
+      card.className = "character-card";
+
+      card.innerHTML = `
+        <img src="${character.image}">
+        <h3>${character.name}</h3>
+        <p>世界観: ${character.world}</p>
+        <p>作者: ${character.author}</p>
+        <p>色: ${character.color}</p>
+      `;
+
+      container.appendChild(card);
+    });
+
+  });
